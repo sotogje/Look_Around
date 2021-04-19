7a3173410e6ece9c90e2a7faee72e7b5430884ae
yosoykyukim <yosoykyukim@gmail.com>
2021-04-19 14:18:32 -0500

    Agregué: - escena de prueba para movimiento de personaje con click - script para movimiento de cámara básico [ViewController] - script para que con un raycast detecte un objeto con tag (teleport) y al dar click lo transporte a la posición del objeto [MovimientoApuntar]

---
+++ Look_Around_A320_Neo/Assets/Scripts/MovimientoApuntar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovimientoApuntar : MonoBehaviour
+{
+    private LayerMask layerMask = 2;
+
+    RaycastHit hit;
+
+    public GameObject player;
+    public GameObject dot_white;
+    public GameObject dot_green;
+
+    private void FixedUpdate()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, (Screen.height / 2), 0));
+
+        Debug.DrawRay(ray.origin, ray.direction * 12, Color.yellow);
+        
+        dot_green.SetActive(false);
+        dot_white.SetActive(true);
+
+        if (Physics.Raycast(ray, out hit, 30))
+        {
+            var selection = hit.transform;
+            
+            Debug.Log(selection.name);
+            
+            if (selection.tag == "teleport")
+            {
+                dot_green.SetActive(true);
+                dot_white.SetActive(false);
+
+                if (Input.GetMouseButton(0))
+                {
+
+                    player.transform.position = selection.transform.position;
+                }
+            }
+
+        }
+    }
+}
